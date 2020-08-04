7cf459eade2a7bf4389e19dcf8f0ebdc91326202
jomarv <jomarverzosa@gmail.com>
2020-08-04 19:02:21 +0800

    Optimise code & DB schema

--- api.php
+++ api.php
@@ -1,7 +1,8 @@
 <?php
 require_once 'db.php';
 
-$poker = $db->query('SELECT p.UserName, HasVote, IF(vote.NoVote > 0, "?", p.Points) AS Points FROM poker p, (SELECT COUNT(*) AS NoVote FROM poker WHERE HasVote = 0) vote');
+$query = 'SELECT p.UserName, HasVote, IF(vote.NoVote > 0, "?", p.Points) AS Points FROM poker p, (SELECT COUNT(*) AS NoVote FROM poker WHERE HasVote = 0) vote';
+$poker = $db->query($query);
 $hasVoted = $poker->fetch_all(MYSQLI_ASSOC);
 
 header('Access-Control-Allow-Origin: *');
